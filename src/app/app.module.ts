3929349324015fc7666dd82ebbf048625a08dfa0
Alex Studer <alex@alexstuder.ch>
2020-12-05 05:14:00 +0100

    2 components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FirstStepsComponent} from './first-steps/first-steps.component';
+import {TryComponent} from './try/try.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstStepsComponent,
+    TryComponent
   ],
   imports: [
     BrowserModule
