e1e01a9356a22b65b550d26d4ec31e55b96c54af
Alex Studer <alex@alexstuder.ch>
2020-12-10 16:18:37 +0100

    *ngIf added

--- src/app/structur-directiven/structur-directiven.component.ts
+++ src/app/structur-directiven/structur-directiven.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-structur-directiven',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StructurDirectivenComponent implements OnInit {
 
-  constructor() { }
+  videos = ['Angular', 'Typescript', 'JavaScript', 'SQL'];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/videos/videos.component.ts
+++ src/app/videos/videos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-videos',
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./videos.component.css']
 })
 export class VideosComponent implements OnInit {
-  videos = ['Angular', 'Typescript', 'JavaScript', 'SQL'];
 
   constructor() { }
 
