edc408fe284e17e5d4815172c8b98787b660680f
Alex Studer at Mac Book Pro <alex@alexstuder.ch>
2020-12-11 07:36:20 +0100

    dependencyInjection added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {StructurDirectivenComponent} from './structur-directiven/structur-directiven.component';
 import {BindingsAndPropertiesComponent} from './bindings-and-properties/bindings-and-properties.component';
+import { DependencyInjectionComponent } from './dependency-injection/dependency-injection.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StructurDirectivenComponent,
-    BindingsAndPropertiesComponent
+    BindingsAndPropertiesComponent,
+    DependencyInjectionComponent
   ],
   imports: [
     BrowserModule
